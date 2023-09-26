74ce0a2fee1af0c76ecf4f16cdb609fb87788e8f
alisosa_meli <ali.sosa@mercadolibre.com>
2023-09-26 20:33:18 -0300

    test

--- src/main/java/com/ucu/topicos/TopicosApplication.java
+++ src/main/java/com/ucu/topicos/TopicosApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TopicosApplication {
 
+	String test = "test";
+
 	public static void main(String[] args) {
 		SpringApplication.run(TopicosApplication.class, args);
 	}
